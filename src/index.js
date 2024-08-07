1d4836836034643c3b1e2762d6af73479c934266
Mariana Macias <sosamariana9573@gmail.com>
2024-08-07 16:20:05 -0600

    compilacion con webpack,añade tareas

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { renderTasks } from "./ui";
+import { addTask } from "./task"; 
+ 
+document.addEventListener("DOMContentLoaded", () => {
+    //Hacemos visible la lista de tareas
+    renderTasks();
+
+    //Agregar el evento para la funcion para agregar tareas
+    document.getElementById("task-form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const taskInput = document.getElementById("task-input");
+    if(taskInput.value !== ""){
+        //Agregamos la tarea
+        addTask(taskInput.value);
+
+        //volvemos a cargar la lista de tareas
+        renderTasks();
+        
+        //Limpiar el input
+        document.getElementById("task-input").value = "";
+      }    
+   });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // punto de entrada de tu aplicacion
-    output:{
-        filename: 'bundle.js', //Nombre del archivo de salidas
-        path: path.resolve(__dirname, 'dist'), // carpeta de salida 
+    mode: 'development', // Para saber si esta en desarrollo o producción
+    entry: './src/index.js', // Punto de entrada de tu aplicación 
+    output: {
+        filename: 'bundle.js', // Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), // Carpeta de salida
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test: /\.css$/, // Regex para identificar archivos css
-                use:['style-loader', 'css-loader'], // Loaders para procesar archivos CSS
+                test: /\.css$/, // Regex para identificar archivos CSS
+                use: ['style-loader', 'css-loader'], // Loaders para procesar archivos CSS
             },
             {
-              test: /\.js$/, //Regex para identificar archivos JS
-              exclude: /node_modules/, //Excluir la carpeta node_modules
-              use:{
-                loader:'babel-loader', // Loader para convertir JS moderno al JS compatible a mas navegadores
-                options: {
-                    presets:['@babel/preset-env'],
+                test: /\.js$/, // Regex para identificar archivos JS
+                exclude: /node_modules/, // Excluir la carpeta node_modules
+                use: {
+                    loader: 'babel-loader', // Loader para convertir JS moderno al JS compatible a más navegadores
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
                 },
             },
+        ],
+    },
+    devtool: 'source-map', // Genera source maps para facilitar la depuración
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'), // Carpeta del que correrá el servidor
         },
-    ] ,
- },
-            devtool:'source-map', // Genera source maps para facilitar la depuracion
-            devserver:{
-                contentBase: path.resolve(__dirname,'dist'),// carpeta del que correra el servidor
-                compress: true, // Habilitar compresion gzip
-                port:9000, // Puerto del servidor de desarrollo
-            },
-};      
+        compress: true, // Habilitar compresión gzip
+        port: 9000, // Puerto del servidor de desarrollo
+    },
+};
